2d40fc9a39dbe3fa0524e3de9df167971af0fcfd
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-09-02 22:33:54 -0600

    continued style changes, add contact and links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import TextAnimation from '../utils/logic';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 function Form() {
@@ -51,6 +53,7 @@ function Form() {
 
   return (
     <div  className="container d-flex flex-column flex-grow-1">
+      
       <div style={{ paddingTop: '30px' }} className="d-flex">
       <TextAnimation />
       </div>
@@ -103,16 +106,17 @@ function Form() {
         </div >
       </form>
       <div style={{ width: '100%' }} className="d-flex flex-grow-1 align-items-end">
-        <ul style={{ color: 'antiquewhite', listStyle: 'none' }}>
-          <li>Contact Information:</li>
-          <li style={{ marginLeft: '25px', fontFamily: 'Lucida Handwriting' }}>Jesse Anderson</li>
-          <li style={{ marginLeft: '25px', fontFamily: 'Sans' }}>LmntryLmnt@gmail.com</li>
-          <li style={{ marginLeft: '25px', fontFamily: 'arial' }}>Third item</li>
+        <ul style={{ color: 'antiquewhite', listStyle: 'none', marginBottom: '60px' }}>
+          <li style={{ fontSize: '22px', fontFamily: 'Sans' }}>Contact Information:</li>
+          <li style={{ marginLeft: '35px', fontFamily: 'Lucida Handwriting' }}>Jesse Anderson</li>
+          <li style={{ marginLeft: '35px', fontFamily: 'Sans' }}>(254)423-5822</li>
+          <li><a style={{ marginLeft: '35px', fontFamily: 'Sans', color: 'antiquewhite', textDecoration: 'none' }} href="https://example.com/link1" target="_blank" rel="noopener noreferrer">LmntryLmnt@gmail.com</a><span><FontAwesomeIcon icon={faGoogle} size="1x" style={{ marginLeft: '5px' }} /></span></li>
+          <li><a style={{ marginLeft: '35px', fontFamily: 'Sans', color: 'antiquewhite', textDecoration: 'none' }} href="https://github.com/Vtencouchclimbr" target="_blank" rel="noopener noreferrer">Vtencouchclimbr</a><span><FontAwesomeIcon icon={faGithub} size="1x" style={{ marginLeft: '5px' }} /></span></li>
+          <li><a style={{ marginLeft: '35px', fontFamily: 'Sans', color: 'antiquewhite', textDecoration: 'none' }} href="https://www.linkedin.com/in/jesse-anderson-991108297/" target="_blank" rel="noopener noreferrer">jesse-anderson-991108297</a><span><FontAwesomeIcon icon={faLinkedin} size="1x" style={{ marginLeft: '5px' }} /></span></li>
         </ul>
       </div>
     </div>
   );
 }
-
 export default Form;
 export const animationMessage = "Thank you for visiting! Please enter your contact information below.↓";
